ce01148f32e02a71bd1f3154c03d0ac7524edf81
shima8823 <taiyaki1275@gmail.com>
2023-05-11 10:40:04 +0900

    gemを追加

---
+++ Assets/Resources/Scripts/Gem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -25)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+
+            // ensure coin moves at the same rate as the skyscrapers do
+            transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0, Space.World);
+        }
+
+        // infinitely rotate this coin about the Y axis in world space
+        transform.Rotate(0, 5f, 0, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // trigger coin pickup function if a helicopter collides with this
+        other.transform.parent.GetComponent<HeliController>().PickupGem();
+        print("destroyed gem");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnGems()
+    {
+        while (true)
+        {
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(4, 8));
+        }
+    }
+}
